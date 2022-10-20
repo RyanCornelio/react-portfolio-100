b1fdd4d6d4e6161f24dd8aa1fe6b7e0be105b1ea
Ryan-Phillips Iserhills Cornelio <ryan.cornelio@gmail.com>
2022-10-20 17:59:52 +0200

    Working on projects

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -3,10 +3,49 @@ import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
-import "react";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@material-ui/icons/School";
 
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2007-2010"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            NTI-Gymnasiet, Göteborg.
+          </h3>
+          <p>EL & IT inriktning: Nätverk.</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2018-2019"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Göteborgs Tekniska College, Torslanda.
+          </h3>
+          <p>El-Mekaniker.</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2020-2022"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Medieinstitutet, Göteborg.
+          </h3>
+          <p>Webbutveckling inom e-handel.</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 };
 
 export default Experience;
