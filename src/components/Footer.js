8b9f8e8e0cd6e470b5a6521ba016cc2495cb4329
Ryan-Phillips Iserhills Cornelio <ryan.cornelio@gmail.com>
2022-10-21 19:34:53 +0200

    Update social media info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from "react";
 import InstagramIcon from "@material-ui/icons/Instagram";
-import TwitterIcon from "@material-ui/icons/Twitter";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import "../styles/Footer.css";
@@ -10,12 +9,17 @@ function Footer() {
     <>
       <div className="footer">
         <div className="socialMedia">
-          <InstagramIcon />
-          <TwitterIcon />
-          <FacebookIcon />
-          <LinkedInIcon />
+          <a href="https://www.instagram.com/ryancornelio_/">
+            <InstagramIcon />
+          </a>
+          <a href="https://www.facebook.com/ryanphillips.cornelio/">
+            <FacebookIcon />
+          </a>
+          <a href="https://www.linkedin.com/in/ryan-phillips-iserhills-cornelio/">
+            <LinkedInIcon />
+          </a>
         </div>
-        <p> &copy; 2022 www.ryancornelio.com </p>
+        <p> &copy; 2022 ryan.cornelio@gmail.com </p>
       </div>
     </>
   );
